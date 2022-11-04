bf018049cac70f9b34bec5020b2734fc5ffb299a
mubarak_hossain <mubarakkha355@gmail.com>
2022-11-04 23:12:42 +0600

    Protecting Route

--- routes/web.php
+++ routes/web.php
@@ -19,10 +19,12 @@ Route::get('/', function () {
 
 //Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
 Route::get('/register',[App\Http\Controllers\CustomRegisterController::class,'registerformshow'])->name('register');
 Route::post('/register',[App\Http\Controllers\CustomRegisterController::class,'registerUser'])->name('register.store');
-
 Route::get('/login',[App\Http\Controllers\CustomRegisterController::class,'loginformShow'])->name('login');
 Route::post('/login',[App\Http\Controllers\CustomRegisterController::class,'loginUser'])->name('login.store');
+Route::middleware(['auth'])->group(function(){
 Route::post('/logout',[App\Http\Controllers\CustomRegisterController::class,'logout'])->name('logout');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+});
